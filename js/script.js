ca6c90cb0a5afe17f68ec4264b5649f4f252852f
unknown <mohasanimam@gmail.com>
2022-03-02 23:41:08 +0600

    done-100

--- js/script.js
+++ js/script.js
@@ -7,14 +7,13 @@ const searchPhone = () => {
     searchField.value = '';
     const productDetilsField = document.getElementById('phone-details')
     productDetilsField.textContent = '';
+    document.getElementById('phone-details').style.display="none"
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchLowerText}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayResults(data.data))
-
 }
-
 // showing results in display 
 
 const displayResults = phones => {
@@ -25,22 +24,23 @@ const displayResults = phones => {
         errorField.innerText = "*** Opps! No result found. Please! Search Again ***";
         errorField.style.color = 'tomato';
         errorField.style.display = "block"
-    }    
+    }     
     else {
         const errorField = document.getElementById('error-message');
         errorField.style.display = "none";
-        phones.forEach(phone => {
+        const arrayOf20phones= phones.slice(0,20);
+        phones.forEach(arrayOf20phones => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
                 <div class="card h-100 w-75 mx-auto p-3 custom-card">
-                        <img src="${phone.image}" class="card-image card-img-top w-75 mx-auto" alt="...">
+                        <img src="${arrayOf20phones.image}" class="card-image card-img-top w-75 mx-auto" alt="...">
                     <div>
                         <div class="card-body">
-                                <h4 class="card-title">${phone.phone_name}</h4>
-                                <p class="card-text">${phone.brand}</p>
+                                <h4 class="card-title">${arrayOf20phones.phone_name}</h4>
+                                <p class="card-text">${arrayOf20phones.brand}</p>
                             <div class='text-end'>
-                                <button class='custom-button' onclick="loadPhoneDetails('${phone.slug}')">View Details</button>  
+                                <button class='custom-button' onclick="loadPhoneDetails('${arrayOf20phones.slug}')">View Details</button>  
                             </div>
                         </div>
                     </div>
@@ -50,8 +50,6 @@ const displayResults = phones => {
         });
     }
 }
-
-// phone.slug problem was not loaded without inverted comma
 
 // loading individual phone details using their id
 
@@ -67,6 +65,7 @@ const loadPhoneDetails = phoneId => {
 const displayProductDetails = details => {
     const productDetilsField = document.getElementById('phone-details')
     productDetilsField.textContent = '';
+    console.log(details?.others?.Bluetooth);
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="row g-0">
@@ -94,11 +93,11 @@ const displayProductDetails = details => {
                                     </li>
                                     <li>
                                         <p class="feature-title fw-bold">
-                                        Sensors:<span class="details fw-light"> ${details.mainFeatures.sensors[0]}, ${details.mainFeatures.sensors[2]}, ${details.mainFeatures.sensors[3]}, ${details.mainFeatures.sensors[4]}, ${details.mainFeatures.sensors[5]}</span>
+                                        Sensors:<span class="details fw-light"> ${details.mainFeatures?.sensors[0] ? details?.mainFeatures?.sensors[0]:''}, ${details.mainFeatures?.sensors[1] ? details?.mainFeatures?.sensors[1]:''}, ${details.mainFeatures?.sensors[2] ? details?.mainFeatures?.sensors[2]:''}, ${details?.mainFeatures?.sensors[3] ? details?.mainFeatures?.sensors[3]:'' }, ${details.mainFeatures?.sensors[4] ? details?.mainFeatures?.sensors[4]:''} ${details.mainFeatures?.sensors[5] ? details?.mainFeatures?.sensors[5]:''}</span>
                                         </p>
                                     </li>
                                     <li>
-                                        <p class="feature-title fw-bold">Others: <span class="details fw-light">${details.others.Bluetooth},</span>
+                                        <p class="feature-title fw-bold">Others: <span class="details fw-light">${details?.others?.Bluetooth ? details.others.Bluetooth:'' } ${details?.others?.GPS  ? details.others.GPS:''}, ${details?.others?.Radio ? details.others.Radio:''}, ${details?.others?.USB ? details.others.USB:''}, ${details?.others?.WLAN ? details.others.WLAN:''} </span>
                                         </p>
                                     </li>
                                 </ul>
@@ -108,6 +107,7 @@ const displayProductDetails = details => {
                 </div>
     `
     productDetilsField.appendChild(div);
+    document.getElementById('phone-details').style.display="block"
 }
 
 
